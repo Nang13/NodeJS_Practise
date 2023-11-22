d13b5e4d9013ee719692e92745caf6e7ddbbd38d
Nắng <99975060+Nang13@users.noreply.github.com>
2023-11-22 13:01:25 +0700

    [Section8]

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,18 +1,18 @@
 'use strict'
 
 const AccessService = require('../services/access.service')
+const { CREATED, OK } = require('../core/success.response')
 class AccessController {
-      signUp = async ( req, res, next ) => {
-            console.log(`[P]::signUp::` , req.body )
-            /**
-             *  200 ok
-             * 201 
-             */
-            return res.status(201).json(
-            await AccessService.signUp(req.body)
-            )
-      
-    }
+      signUp = async (req, res, next) => {
+
+            new CREATED({
+                  message: 'Registered Ok:',
+                  metadata: await AccessService.signUp(req.body),
+                  options: {
+                        limit: 10
+                  }
+            }).send(res)
+      }
 }
 
 
